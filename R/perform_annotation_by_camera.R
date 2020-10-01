cb9033c4e342f1c1b305c7213493822026e6f00e
sunil20dhakad <sunil.dhakad@elucidata.io>
2020-10-01 13:47:36 +0530

    [WIP]: add camera run function

---
+++ R/perform_annotation_by_camera.R
@@ -0,0 +1,43 @@
+#' perform_annotation_by_camera
+#'
+#' It performs annotation by CAMERA using different functions of it.
+#'
+#' @param xcms_object The xcms object having peaks information
+#' @param polarity The polarity of the data
+#' @param ppm General ppm error
+#' @param mzabs General absolute error in m/z
+#' @param adducts_rules A dataframe with different adducts rules
+#' @param cor_exp_th Threshold for intensity correlations across samples
+#' @param pval p-value threshold for testing correlation of significance (0-1)
+#' @param perfwhm percentage of FWHM (Full Width at Half Maximum) width used in groupFWHM function for grouping features
+#' @param sigma multiplier of the standard deviation used in groupFWHM function for grouping features
+#' @param maxcharge maximum ion charge
+#' @param maxiso maximum number of expected isotopes (0-8)
+#' @param minfrac The percentage number of samples, which must satisfy the C12/C13 rule for isotope annotation
+#' @param multiplier If no ruleset is provided, calculate ruleset with max. number n of [nM+x] cluster ions
+#' @param max_peaks How many peaks will be calculated in every thread using the parallel mode
+#' @return xcms object
+#' @examples
+#' create_xcms_object_from_elmaven(maven_output_df)
+#' @import CAMERA dplyr stringr
+#' @export
+perform_annotation_by_camera <- function(xcms_object, polarity = NULL, ppm = 5, mzabs = 0.01,
+                                         adducts_rules = NULL, cor_exp_th = 0.75, pval = 0.05,
+                                         perfwhm = 0.6, sigma = 6, maxcharge = 3, maxiso = 4, 
+                                         minfrac = 0.5, multiplier = 3, max_peaks = 100){
+  message("CAMERA Run Started...")
+  require(CAMERA)
+  require(dplyr)
+  require(stringr)
+  
+  an <- xsAnnotate(xcms_object)
+  anF <- groupFWHM(an, sigma = sigma, perfwhm = perfwhm, intval = "maxo")
+  anIC <- groupCorr(anF, calcCiS = FALSE, calcCaS = TRUE, cor_exp_th = cor_exp_th, pval = pval, intval = "maxo")
+  anI <- findIsotopes(anIC, maxcharge = maxcharge, maxiso = maxiso, ppm = ppm, mzabs = mzabs, intval = "maxo", minfrac = minfrac)
+  anFA <- findAdducts(anI, ppm = ppm, mzabs = mzabs,  multiplier = multiplier, polarity = polarity, rules = adducts_rules, max_peaks = max_peaks, intval = "maxo")
+  annotated_peaks_df <- getPeaklist(anFA)
+  
+  message("CAMERA Run Completed...")
+  
+  return(annotated_peaks_df)
+}
