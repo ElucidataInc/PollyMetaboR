81290c43b1a09f1bda83db5e8309fc4205f44343
sunil20dhakad <sunil.dhakad@elucidata.io>
2020-10-06 15:11:40 +0530

    [WIP]: add function to smartbind

---
+++ R/smartbind_dataframes.R
@@ -0,0 +1,40 @@
+#' smartbind_dataframes
+#'
+#' It combines two dataframe having same columns
+#'
+#' @param df_1 The dataframe 1
+#' @param df_2 The dataframe 2
+#' @return The combined dataframe
+#' @examples
+#' smartbind_dataframes(df_1, df_2)
+#' @import dplyr gtools
+#' @export
+smartbind_dataframes <- function(df_1 = NULL, df_2 = NULL){
+      
+  message("Smartbind Dataframe Started...")
+  require(dplyr)
+  require(gtools)
+     
+  if (identical(df_1, NULL) && identical(df_2, NULL)){
+    warning("Both dataframes are NULL\n")
+    combined_df <- NULL
+  } 
+  else if ((class(df_1) == 'data.frame') && (class(df_2) == 'data.frame')){
+    combined_df <- gtools::smartbind(df_1, df_2)
+    rownames(combined_df) <- 1:length(rownames(combined_df))
+  }
+  else if ((class(df_1) == 'data.frame') && (class(df_2) != 'data.frame')){
+    combined_df <- df_1
+  }
+  else if ((class(df_1) != 'data.frame') && (class(df_2) == 'data.frame')){
+    combined_df <- df_2
+  }
+  else{
+    warning("Please provide vaild dataframes\n")
+    combined_df <- NULL
+  }
+
+  message("Smartbind Dataframe Completed...")
+  
+  return (combined_df)    
+}
