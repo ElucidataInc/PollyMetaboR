9adfd3bdca977c4669295b79950e6297859a756e
sunil20dhakad <sunil.dhakad@elucidata.io>
2020-12-15 15:03:55 +0530

    [WIP]: add identification score col in maven output

--- R/make_group_summary_from_metab_ident_format.R
+++ R/make_group_summary_from_metab_ident_format.R
@@ -22,18 +22,25 @@ make_group_summary_from_metab_ident_format <- function(metab_identified_df = NUL
     return (NULL)
   }
   
-  default_cols  <- c('mz', 'mzmin', 'mzmax', 'rt', 'rtmin', 'rtmax', 'groupId',
-                     'identified', 'isotopes', 'adduct', 'pcgroup', 'adduct_type',
-                     'basemass', 'isotope_id', 'isotope_type', 'feature_group',
-                     'compound', 'id', 'formula', 'mass')
-  utp_output_cols <- colnames(metab_identified_df)
+  metab_default_cols  <- c('mz', 'mzmin', 'mzmax', 'rt', 'rtmin', 'rtmax', 'groupId',
+                           'identified', 'isotopes', 'adduct', 'pcgroup', 'adduct_type',
+                           'basemass', 'isotope_id', 'isotope_type', 'feature_group',
+                           'compound', 'id', 'formula', 'mass', 'rt_db', 'identification_score')
   
-  if (!all(default_cols %in% utp_output_cols)){
-    if (!all(default_cols[1:16] %in% utp_output_cols)){
+  maven_default_cols <- c("label", "metaGroupId", "groupId", "goodPeakCount", "medMz",
+                          "medRt", "maxQuality", "adductName", "isotopeLabel", "compound",
+                          "compoundId", "formula", "expectedRtDiff", "ppmDiff", "parent")
+  
+  subset_cols <- c("groupId", "mz", "rt", "compound",  "id", "formula", "feature_group",
+                   "adduct_type", "isotope_type", "basemass")    
+  
+  utp_output_cols <- colnames(metab_identified_df)
+  if (!all(metab_default_cols %in% utp_output_cols)){
+    if (!all(metab_default_cols[1:16] %in% utp_output_cols)){
       warning(c("The metab_identified_df should have at least the following columns : ",
-                paste0(default_cols[1:16], collapse = ", "),
+                paste0(metab_default_cols[1:16], collapse = ", "),
                 "\nThe following columns are optional which are from metabolite identification step : ",
-                paste0(default_cols[17:20], collapse = ", ")))
+                paste0(metab_default_cols[17:20], collapse = ", ")))
       return (NULL)
     }
     
@@ -47,13 +54,17 @@ make_group_summary_from_metab_ident_format <- function(metab_identified_df = NUL
     
     if (!("formula" %in% utp_output_cols)){
       metab_identified_df$formula <- NA
-    }
+    }      
   }
   
-  samples_vec <- utp_output_cols[!utp_output_cols %in% default_cols]
-  subset_cols <- c("groupId", "mz", "rt", "compound",  "id", "formula", "feature_group",
-                   "adduct_type", "isotope_type", "basemass", samples_vec)
-  maven_output_format <- metab_identified_df[, subset_cols, drop = FALSE]
+  if ("identification_score" %in% utp_output_cols){
+    maven_default_cols <- c(maven_default_cols, "identification_score")
+    subset_cols <- c(subset_cols, "identification_score")
+  }
+  
+  samples_vec <- utp_output_cols[!utp_output_cols %in% metab_default_cols]
+  filter_cols <- c(subset_cols, samples_vec)
+  maven_output_format <- metab_identified_df[, filter_cols, drop = FALSE]
   maven_output_format  <- dplyr::rename_at(maven_output_format, dplyr::vars(c("mz","rt", "id")), ~ c("medMz", "medRt", "compoundId"))
   maven_output_format  <- dplyr::mutate(maven_output_format, label = "", goodPeakCount= "",
                                         maxQuality="", adductName="", isotopeLabel="",
@@ -62,9 +73,7 @@ make_group_summary_from_metab_ident_format <- function(metab_identified_df = NUL
   maven_output_format$adductName <- maven_output_format$adduct_type
   maven_output_format$isotopeLabel <- maven_output_format$isotope_type
   maven_output_format$parent <- maven_output_format$basemass    
-  maven_default_cols <- c("label", "metaGroupId", "groupId", "goodPeakCount", "medMz",
-                          "medRt", "maxQuality", "adductName", "isotopeLabel", "compound",
-                          "compoundId", "formula", "expectedRtDiff", "ppmDiff", "parent")
+  
   maven_cols_order <- c(maven_default_cols, samples_vec)
   maven_output_format <- maven_output_format[, maven_cols_order, drop = FALSE]
   cmpd_nan_bool <- is.na(maven_output_format$compound)
