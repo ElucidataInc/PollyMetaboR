eb4e6a9f1bdc73ad256ade9640a71bc41e133056
sunil20dhakad <sunil.dhakad@elucidata.io>
2021-07-01 18:14:49 +0530

    [WIP]: optimzed read_msp performance

--- R/read_msp.R
+++ R/read_msp.R
@@ -11,9 +11,9 @@
 #' @export
 read_msp <- function(msp_path = NULL, compound = NULL, exact_match = TRUE){
   message("Read MSP Started...")
-  require(data.table)
-  require(dplyr)
-  require(stringr)
+  require(readr)  
+  require(stringi)  
+  require(stringr)  
   
   if (identical(msp_path, NULL)){
     warning("The msp_path is NULL")
@@ -34,92 +34,104 @@ read_msp <- function(msp_path = NULL, compound = NULL, exact_match = TRUE){
     if (identical(exact_match, NULL)){ exact_match <- TRUE}  
   }
   
-  file_con  <- file(msp_path, open = "r")
-  msp_list <- list()
-  name_list <- list()
   id_list <- list()
-  name_index <- 0
-  name_bool <- FALSE
-  num_peaks <- 0
-  count_peaks <- 0
-  while (length(one_line <- readLines(file_con, n = 1, warn = FALSE)) > 0){
-    try({
-      if (identical(compound, NULL)){
-        if (grepl("Name: ", one_line, fixed = FALSE, ignore.case = TRUE)){
-          name_list <- list()
-          spectrum_list <- NULL
-          name_index <- name_index + 1
-          name_bool <- TRUE
-          num_peaks <- 0
-          count_peaks <- 0
+  msp_list <- list()
+  
+  data_as_str <- readr::read_file(msp_path)
+  compound_data_list <- strsplit(gsub("Name: ","~Name: ", data_as_str, ignore.case = T), split = "~")[[1]]
+  if (!identical(compound, NULL)){
+    compound_data_list <- compound_data_list[sapply(compound_data_list, function(x) any(sapply(compound, function(y) any(grepl(y, x, fixed = TRUE)))))]
+  }
+  compound_data_list <- compound_data_list[lengths(compound_data_list) > 0 & compound_data_list != ""] 
+  
+  get_compound_msp <- function(element_msp = NULL){
+    compound_msp <- list()
+    element_msp <- stringi::stri_split_lines(element_msp)[[1]]
+    names_bool <- sapply(element_msp, function(x) grepl(": ", x, fixed = TRUE))
+    names_elements <- element_msp[names_bool]
+    if (length(names_elements) > 0){
+      names_split_list <- lapply(names_elements, function(x){
+        name_list <- list()
+        name_split <- stringr::str_trim(stringr::str_split(x, ": ", 2)[[1]])
+        num_val <- suppressWarnings(as.numeric(name_split[2]))
+        if (!is.na(num_val)){
+          name_list[[name_split[1]]] <- num_val
+        }
+        else {
+          name_list[[name_split[1]]] <- name_split[2]
+        }
+        return (name_list) 
+      })
+      if (length(names_split_list) > 0){
+        for (name_element in names_split_list){
+          compound_msp <- c(compound_msp, name_element)
         }
       }
-      else {
-        compound <- stringr::str_trim(compound)
-        if (any(sapply(compound, function(x) grepl(x, one_line, fixed = TRUE))) && grepl("Name: ", one_line, fixed = FALSE, ignore.case = TRUE)){
-          if (identical(exact_match, FALSE)){
-            name_list <- list()
-            spectrum_list <- NULL
-            name_index <- name_index + 1
-            name_bool <- TRUE
-            num_peaks <- 0
-            count_peaks <- 0
-          }
-          else {
-            split_line <- stringr::str_trim(stringr::str_split(one_line, ": ", 2)[[1]])
-            if (any(split_line[2] %in% compound)){
-              name_list <- list()
-              spectrum_list <- NULL
-              name_index <- name_index + 1
-              name_bool <- TRUE
-              num_peaks <- 0
-              count_peaks <- 0
-            }
-          }
+    }
+    
+    peaks_elements <- element_msp[!names_bool]
+    if (length(peaks_elements) > 0){                        
+      peaks_split_list <- lapply(peaks_elements, function(x) {
+        peak_list <- list()
+        peak_split <- stringr::str_trim(base::strsplit(x, "(\\s+|\\t+)(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))", perl = TRUE)[[1]])
+        return (peak_split)
+      })
+      peaks_split_list <- peaks_split_list[lengths(peaks_split_list) > 0 & peaks_split_list != ""]
+      if (length(peaks_split_list) > 0){                      
+        if (length(peaks_split_list) > 1){
+          peaks_df <- as.data.frame(stringi::stri_list2matrix(peaks_split_list, byrow=TRUE), stringsAsFactors = FALSE)
+        }
+        else {
+          peaks_df <- as.data.frame(t(data.frame(peaks_split_list)), stringsAsFactors = FALSE)
+        }
+        elements_len <- ncol(peaks_df)
+        if (elements_len > 1){
+          extra_cols_len <- elements_len - 2
+          if (extra_cols_len > 1){ extra_cols <- paste("other_info", c(1:extra_cols_len), sep = "_")}
+          else if (extra_cols_len == 1){ extra_cols <- "other_info"}
+          else {extra_cols <- NULL}
+          spectrum_cols <- c("mz", "intensity")
+          colnames(peaks_df)<- c(spectrum_cols, extra_cols)
+          peaks_df[, spectrum_cols] <- suppressWarnings(sapply(peaks_df[, spectrum_cols], as.numeric))
+          row.names(peaks_df) <- NULL   
+          compound_msp[["pspectrum"]] <- peaks_df
         }
       }  
-      
-      if (name_bool){
-        if (grepl(": ", one_line, fixed = TRUE)){
-          split_line <- stringr::str_trim(stringr::str_split(one_line, ": ", 2)[[1]])
-          num_val <- suppressWarnings(as.numeric(split_line[2]))
-          if (!is.na(num_val)){
-            name_list[[split_line[1]]] <- num_val
-            if (grepl(paste(c("Num Peaks", "NumPeaks"), collapse = "|"), split_line[1],  fixed = FALSE, ignore.case = TRUE)){
-              num_peaks <- num_val
+    }                       
+    
+    return (compound_msp)             
+  }                                                                                           
+  
+  name_index <- 0                                                                                       
+  for (compound_data in compound_data_list){
+    compound_msp <- get_compound_msp(compound_data)
+    if (length(compound_msp) > 0){
+      if ("Name" %in% names(compound_msp)){
+        if (identical(compound, NULL)){
+          name_index <- name_index + 1
+          msp_list[[name_index]] <- compound_msp
+          id_list[[name_index]] <- compound_msp$Name 
+        }
+        else {
+          
+          if (identical(exact_match, FALSE)){ 
+            name_index <- name_index + 1
+            msp_list[[name_index]] <- compound_msp
+            id_list[[name_index]] <- compound_msp$Name  
+          }
+          else {
+            if (any(compound_msp$Name %in% compound)){
+              name_index <- name_index + 1
+              msp_list[[name_index]] <- compound_msp
+              id_list[[name_index]] <- compound_msp$Name  
             }
           }
-          else{ name_list[[split_line[1]]] <- split_line[2] }
         }
-        else{
-          if (num_peaks > 0){
-            split_line <- stringr::str_trim(base::strsplit(one_line, "(\\s+|\\t+)(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))", perl = TRUE)[[1]]) 
-            elements_len <- length(split_line)
-            if (elements_len > 1){
-              extra_cols_len <- elements_len - 2
-              if (extra_cols_len > 1){ extra_cols <- paste("other_info", c(1:extra_cols_len), sep = "_")}
-              else if (extra_cols_len == 1){ extra_cols <- "other_info"}
-              else {extra_cols <- NULL}
-              spectrum_cols <- c("mz", "intensity")
-              names(split_line) <- c(spectrum_cols, extra_cols)
-              interm_df <- as.data.frame(dplyr::bind_rows(split_line), stringsAsFactors = FALSE)
-              interm_df[, spectrum_cols] <- sapply(interm_df[, spectrum_cols], as.numeric)           
-              spectrum_list <- as.data.frame(data.table::rbindlist(list(spectrum_list, interm_df), fill = TRUE), stringsAsFactors = FALSE)
-              name_list[["pspectrum"]] <- spectrum_list
-            }
-            count_peaks <- count_peaks + 1 
-            if (count_peaks >= num_peaks){ name_bool <- FALSE}
-          }
-        }
-        msp_list[[name_index]] <- name_list
-        id_list[[name_index]] <- name_list[[1]]
       }
-      
-    }, silent = TRUE)                     
+    }
   }
-  close(file_con)
   
-  message("Read MSP Completed...")
+  message("Read MSP Completed...")                                                                                      
   
-  return (list(compound = id_list, msp = msp_list))
+  return (list(compound = id_list, msp = msp_list))                                                                                           
 }
