5c28436d1dd5efc56490c775cab4bc6d9be3fe87
Sunil Dhakad <sunil.dhakad@elucidata.io>
2022-09-27 16:15:09 +0530

    [WIP]: add ignore case parameter

--- R/get_elements_from_msp.R
+++ R/get_elements_from_msp.R
@@ -1,15 +1,16 @@
 #' get_elements_from_msp
 #'
-#' It returns the list of elements found in the msp file using the compound key
+#' It converts the string text of a single compound msp into the list of msp data for a compound
 #'
 #' @param msp_path The path to msp file
 #' @param compound_key A key name from msp file where features/compounds are present
+#' @param ignore_case Ignore case while searching compound key
 #' @param load_on_memory Load whole text into a list if TRUE else read lines one by one to save memory
 #' @return A vector of features/compounds
 #' @examples
 #' get_elements_from_msp(msp_path, compound_key)
 #' @export
-get_elements_from_msp <- function(msp_path = NULL, compound_key = NULL, load_on_memory = TRUE){
+get_elements_from_msp <- function(msp_path = NULL, compound_key = NULL, ignore_case = FALSE, load_on_memory = TRUE){
   message("Get Elements From MSP Started...")
   require(readr)
   require(stringr)  
@@ -34,21 +35,34 @@ get_elements_from_msp <- function(msp_path = NULL, compound_key = NULL, load_on_
     return (NULL)
   }
   
+  ignore_case <- as.logical(ignore_case)
+  if (is.na(ignore_case)){
+    warning("The ignore_case should be TRUE/FALSE ")
+    return (NULL)
+  }
+  
   load_on_memory <- as.logical(load_on_memory)
   if (is.na(load_on_memory)){
     warning("The load_on_memory should be TRUE/FALSE ")
     return (NULL)
-  } 
+  }    
   
-  if (identical(load_on_memory, TRUE)){
+  if (identical(ignore_case, TRUE)){
+    fixed_search <- FALSE
+  } else {
+    fixed_search <- TRUE
+  }
+  
+  compound_key <- paste(compound_key, ":", sep = "")  
+  if (identical(load_on_memory, TRUE)){  
     data_as_readlines <- readr::read_lines(msp_path)
-    compound_key_list <- data_as_readlines[sapply(data_as_readlines, function(x) grepl(pattern = paste(compound_key, ":", sep = ""), x = x, ignore.case = TRUE, fixed = FALSE))]                                              
+    compound_key_list <- data_as_readlines[sapply(data_as_readlines, function(x) grepl(pattern = compound_key, x = x, ignore.case = ignore_case, fixed = fixed_search))]                                              
     compound_values  = as.character(sapply(compound_key_list, function(x) stringr::str_trim(stringr::str_split(x, ":", 2)[[1]][2])))
   } else {
     compound_values <- c()
     file_con  <- file(msp_path, open = "r")
     while (length(one_line <- readLines(file_con, n = 1, warn = FALSE)) > 0){
-      if(grepl(pattern = paste(compound_key, ":", sep = ""), x = one_line, ignore.case = TRUE, fixed = FALSE)){
+      if(grepl(pattern = compound_key, x = one_line, ignore.case = ignore_case, fixed = fixed_search)){
         element_val <- stringr::str_trim(stringr::str_split(one_line, ":", 2)[[1]])[2]
         compound_values <- c(compound_values, element_val)                                 
       }
